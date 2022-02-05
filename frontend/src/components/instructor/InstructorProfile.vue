c4711e0cd8cec6e34295f2e1ae436d0686f67611
Hui-Story <leejaehee0728@gmail.com>
2022-02-05 21:33:54 +0900

    ENG: update InstructorProfile layout
    강사 프로필 페이지 레이아웃 작성

---
+++ frontend/src/components/instructor/InstructorProfile.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="instructor-profile">
+    <div class="instructor-profile-img">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+    </div>
+    <div class="instructor-profile-info">
+      <h6 class="q-mb-xs text-bold">아이유</h6>
+      <div>이메일 : iu@gmail.com</div>
+      <div>강좌 수 : 200</div>
+      <div>별점 : ★5.0 (200000)</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+
+export default {
+  name: 'InstructorProfile',
+  components: {
+  },
+
+  setup() {
+    const state = reactive({
+    })
+
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style scoped>
+.instructor-profile {
+  position: absolute;
+  top: 100px;
+  left: 60px;
+  width: 380px;
+  height: 150px;
+}
+
+.instructor-profile-img {
+  border-radius: 50px;
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+}
+
+.instructor-profile-img > img {
+  position: relative;
+  height: 100%;
+  top: 50%;
+  left: 50%;
+  z-index: 2;
+  transform: translate(-50%, -50%);
+}
+
+.instructor-profile-info {
+  position: absolute;
+  top: 10px;
+  right: 0px;
+  z-index: 1;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 8px;
+  width: 300px;
+  height: 130px;
+  padding: 12px 0px 0px 100px;
+  color: white;
+}
+</style>
--- frontend/src/views/instructor/InstructorDetailView.vue
+++ frontend/src/views/instructor/InstructorDetailView.vue
@@ -1,13 +1,105 @@
-<template lang="">
-  <div>
-    <h1>강사 프로필 페이지</h1>
+<template>
+  <div class="header">
+    <instructor-header></instructor-header>
+  </div>
+  <div class="contents column q-px-xl q-mx-lg q-pt-lg q-pb-xl">
+    <div class="col-4 row q-pb-sm">
+      <div class="instructor-description-block col-6 column q-pr-lg">
+        <div class="contents-header col-auto">
+          <span class="text-h6 text-bold">강사 소개</span>
+        </div>
+        <div class="col">
+          <div>네이버, 카카오, 라인플러스, 쿠팡, 배달의 민족, 당근마켓, 토스를 거친 AI분야 세계 최고 권위자. 네이버, 카카오, 라인플러스, 쿠팡, 배달의 민족, 당근마켓, 토스를 거친 AI분야 세계 최고 권위자.</div>
+        </div>
+      </div>
+      <div class="instructor-review-block col-6 column q-pl-lg">
+        <div class="contents-header col-auto">
+          <span class="contents-title">수강후기</span>
+          <span class="contents-more">더보기></span>
+        </div>
+        <div class="instructor-review-list col">
+
+        </div>
+      </div>
+    </div>
+    <div class="in-progress-course-block col-4 column q-py-sm">
+      <div class="contents-header col-auto">
+        <span class="contents-title">진행중인 강좌</span>
+        <span class="contents-more">더보기></span>
+      </div>
+      <div class="in-progress-course-list col">
+
+      </div>
+    </div>
+    <div class="all-course-block col-4 column q-py-sm">
+      <div class="contents-header col-auto">
+        <span class="contents-title">전체 강좌</span>
+        <span class="contents-more">더보기></span>
+      </div>
+      <div class="all-course-list col">
+
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
+import { reactive } from '@vue/reactivity'
+import InstructorHeader from "@/components/instructor/InstructorHeader"
+
 export default {
-  
+  name: 'InstructorDetailView',
+  components: {
+    InstructorHeader,
+  },
+
+  setup() {
+    const state = reactive({
+    })
+
+    return {
+      state
+    }
+  }
 }
 </script>
-<style lang="">
-  
+
+<style scoped>
+.header {
+  height: 25vh;
+}
+
+.contents {
+  height: 75vh;
+}
+
+.contents-header {
+  margin-bottom: 0.5vh;
+}
+
+.contents-title {
+  font-size: 1.5vh;
+  font-weight: bold;
+}
+
+.contents-more {
+  font-size: 1.2vh;
+  color: blue;
+  margin-left: 0.4vw;
+}
+
+.instructor-review-list {
+  background-color: lightgray;
+  border-radius: 0.5vh;
+}
+
+.in-progress-course-list {
+  background-color: lightblue;
+  border-radius: 0.5vh;
+}
+
+.all-course-list {
+  background-color: lightblue;
+  border-radius: 0.5vh;
+}
 </style>
