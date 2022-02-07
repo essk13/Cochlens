fe37af134b3daa0b63b7a7fef114492f1263dcda
Hui-Story <leejaehee0728@gmail.com>
2022-02-07 14:14:01 +0900

    ENG: add live-test button
    rtc 테스트 버튼 추가

--- frontend/src/views/course/CourseDetailView.vue
+++ frontend/src/views/course/CourseDetailView.vue
@@ -1,13 +1,28 @@
 <template lang="">
   <div>
-    
+    <input onclick="register(); return false;" type="button" name="join" value="Join!">
   </div>
 </template>
+
 <script>
+import { reactive } from '@vue/reactivity'
+
 export default {
-  
+  name: 'HomeView',
+  components: {
+  },
+
+  setup() {
+    const state = reactive({
+    })
+
+    return {
+      state,
+    }
+  }
 }
 </script>
-<style lang="">
+
+<style>
   
 </style>
