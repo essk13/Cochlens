bc3d57b892f615e777507bc5403d0e49e559698b
ek_13 <kldoggod@gmail.com>
2022-02-14 02:59:37 +0900

    CLN: Delete trash files & Update code
    필요없는 파일 제거 및 코드 정리

--- frontend/public/detection.js
+++
@@ -1,6 +0,0 @@
-Promise.all([
-  faceapi.loadFaceRecognitionModel('/models'),
-  faceapi.loadFaceLandmarkModel('/models'),
-  faceapi.loadTinyFaceDetectorModel('/models'),
-  faceapi.loadFaceExpressionModel('/models')
-]).then(console.log('success')).catch(console.log('err'))
